193a52534c7b0ba42ce86601d9126aa2d9b260cf
Nick Reardon <32754140+NickReardon@users.noreply.github.com>
2026-05-05 00:49:43 -0700

    Implemented model training and data preprocessing in ml.py

--- ml.py
+++ ml.py
@@ -1,3 +1,29 @@
 # CPSC 481 - Logistics Robot Decision Agent ml.py
-# File Author:
+# File Author: Nicholas Reardon
 
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
+
+def load_and_train_model():
+    data = pd.read_csv('logistics_robot_data.csv')
+    
+    encoders = {}
+    for column in data.columns:
+        label_encoder = LabelEncoder()
+        data[column] = label_encoder.fit_transform(data[column]) # type: ignore
+        encoders[column] = label_encoder
+        
+        
+    feature_columns = ["congestion_level", "delay_level", "distance", "time_of_day", "zone_type"]
+    X = data[feature_columns]
+    y = data["delay_level"]
+
+    model = DecisionTreeClassifier(random_state=42)
+    model.fit(X, y)
+
+    return model, encoders
+
+
+
+MODEL, ENCODERS = load_and_train_model()
