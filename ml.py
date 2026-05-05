cbe9502ca528087b29c66ce713ffbeb09e7f7a70
Nick Reardon <32754140+NickReardon@users.noreply.github.com>
2026-05-05 00:58:00 -0700

    Added function to extract route features

--- ml.py
+++ ml.py
@@ -24,6 +24,42 @@ def load_and_train_model():
 
     return model, encoders
 
+def extract_route_features(route, grid, time_of_day):
+    steps = len(route) # maybe needs to be -1
+    
+    # Define distance categories based on the number of steps
+    if steps <= 6:
+        distance = "short"
+    elif steps <= 10:
+        distance = "medium"
+    else:
+        distance = "long"
+        
+    # look up what symbol is at each cell in the route
+    route_cells = [grid[row][column] for row, column in route]
+    
+    #classify the route based on worst cell type in the route, R > H > normal
+    if "R" in route_cells:
+        zone_type = "restricted"
+    elif "H" in route_cells:
+        zone_type = "high_traffic"
+    else:
+        zone_type = "normal"
+        
+    # classify congestion level based on number of high traffic cells in the route
+    high_traffic_count = route_cells.count("H")
+    if high_traffic_count == 0:
+        congestion_level = "low"
+    elif high_traffic_count <= 2:
+        congestion_level = "medium"
+    else:
+        congestion_level = "high"
+    
+    
+    return time_of_day, congestion_level, distance, zone_type
+
+
+
 
 
 MODEL, ENCODERS = load_and_train_model()
